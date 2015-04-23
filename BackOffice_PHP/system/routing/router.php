a7ed3587c7f5957284de66f1b2caaa3c92ab63ce
samuelfr <samuel.frigout@gmail.com>
2015-04-23 13:13:15 +0200

    Add abstract Route

--- BackOffice_PHP/system/routing/router.php
+++ BackOffice_PHP/system/routing/router.php
@@ -59,4 +59,29 @@ class Router
 
         return $this;
     }
+
+
+    /**
+     * @param Request $request
+     *
+     * @return Route|null
+     */
+    public function dispatch(Request $request)
+    {
+        $this->setOriginalRequest($request);
+        $redispatch = false;
+        $routeSelected = null;
+        do {
+            foreach ($this->routes as $route) {
+                if ($route->setRequest($request)->canHandle()) {
+                    $route->handle();
+                    $redispatch = $route->hasToBeReDispatched();
+                    $routeSelected = $route;
+                    break;
+                }
+            }
+        } while ($redispatch);
+
+        return $routeSelected;
+    }
 }
