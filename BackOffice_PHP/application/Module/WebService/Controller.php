29ec302dbed2869bdf523e5959d5ec804714de25
samuelfr <samuel.frigout@gmail.com>
2015-06-09 16:07:53 +0200

    WebService - getUpdate with no date

--- BackOffice_PHP/application/Module/WebService/Controller.php
+++ BackOffice_PHP/application/Module/WebService/Controller.php
@@ -15,78 +15,76 @@ class Controller extends \FSF\Controller
         $date = Date::blockToFr($date);
 
         $json_array = array(
-            "themes_nouveaux" => array(),
-            "themes_modifies" => array(),
-            "examens_nouveaux" => array(),
-            "examens_modifies" => array(),
-            "questions_nouvelles" => array(),
-            "questions_modifiees" => array(),
-            "themes_supprimes" => array(),
-            "examens_supprimes" => array(),
+            "themes_nouveaux"      => array(),
+            "themes_modifies"      => array(),
+            "examens_nouveaux"     => array(),
+            "examens_modifies"     => array(),
+            "questions_nouvelles"  => array(),
+            "questions_modifiees"  => array(),
+            "themes_supprimes"     => array(),
+            "examens_supprimes"    => array(),
             "questions_supprimees" => array(),
-            "cours_nouveaux" => array(),
-            "cours_supprimes" => array(),
+            "cours_nouveaux"       => array(),
+            "cours_supprimes"      => array(),
         );
 
-        if($date != "") {
-            // Themes
-            $theme_model = new ThemeModel();
-            $themes_modifies = $theme_model->getUpdatedThemes($date);
-            foreach ($themes_modifies as $theme_modif) {
-                /** @var \APP\Entity\Theme $theme_modif */
-                $json_array['themes_modifies'][] = $theme_modif->toArray();
-            }
-            $themes_nouveaux = $theme_model->getNewThemes($date);
-            foreach ($themes_nouveaux as $theme_nouveau) {
-                /** @var \APP\Entity\Theme $theme_nouveau */
-                $json_array['themes_nouveaux'][] = $theme_nouveau->toArray();
-            }
-            $themes_supprimes = $theme_model->getDeletedThemes($date);
-            foreach ($themes_supprimes as $theme_suppr) {
-                /** @var \APP\Entity\Theme $theme_suppr */
-                $json_array['themes_supprimes'][] = $theme_suppr->getIdTheme();
-            }
-
-            // Examens
-            $examen_model = new ExamenModel();
-            $examens_modifies = $examen_model->getUpdatedExamens($date);
-            foreach ($examens_modifies as $examen_modif) {
-                /** @var \APP\Entity\Examen $examen_modif */
-                $json_array['examens_modifies'][] = $examen_modif->toArray();
-            }
-            $examens_nouveaux = $examen_model->getNewExamens($date);
-            foreach ($examens_nouveaux as $examen_nouveau) {
-                /** @var \APP\Entity\Examen $examen_nouveau */
-                $json_array['examens_nouveaux'][] = $examen_nouveau->toArray();
-            }
-            $examens_supprimes = $examen_model->getDeletedExamens($date);
-            foreach ($examens_supprimes as $examen_suppr) {
-                /** @var \APP\Entity\Examen $examen_suppr */
-                $json_array['examens_supprimes'][] = $examen_suppr->getIdExamen();
-            }
-
-            // Questions
-            $question_model = new QuestionModel();
-            $questions_modifies = $question_model->getUpdatedQuestions($date);
-            foreach ($questions_modifies as $question_modif) {
-                /** @var \APP\Entity\Question $question_modif */
-                $json_array['questions_modifiees'][] = $question_modif->toArray();
-            }
-            $questions_nouveaux = $question_model->getNewQuestions($date);
-            foreach ($questions_nouveaux as $question_nouveau) {
-                /** @var \APP\Entity\Question $question_nouveau */
-                $json_array['questions_nouvelles'][] = $question_nouveau->toArray();
-            }
-            $questions_supprimes = $question_model->getDeletedQuestions($date);
-            foreach ($questions_supprimes as $question_suppr) {
-                /** @var \APP\Entity\Question $question_suppr */
-                $json_array['questions_supprimees'][] = $question_suppr->getIdQuestion();
-            }
-
-            // Cours
-
+        // Themes
+        $theme_model = new ThemeModel();
+        $themes_modifies = $theme_model->getUpdatedThemes($date);
+        foreach ($themes_modifies as $theme_modif) {
+            /** @var \APP\Entity\Theme $theme_modif */
+            $json_array['themes_modifies'][] = $theme_modif->toArray();
+        }
+        $themes_nouveaux = $theme_model->getNewThemes($date);
+        foreach ($themes_nouveaux as $theme_nouveau) {
+            /** @var \APP\Entity\Theme $theme_nouveau */
+            $json_array['themes_nouveaux'][] = $theme_nouveau->toArray();
+        }
+        $themes_supprimes = $theme_model->getDeletedThemes($date);
+        foreach ($themes_supprimes as $theme_suppr) {
+            /** @var \APP\Entity\Theme $theme_suppr */
+            $json_array['themes_supprimes'][] = $theme_suppr->getIdTheme();
         }
 
-        return json_encode($json_array, JSON_NUMERIC_CHECK );
+        // Examens
+        $examen_model = new ExamenModel();
+        $examens_modifies = $examen_model->getUpdatedExamens($date);
+        foreach ($examens_modifies as $examen_modif) {
+            /** @var \APP\Entity\Examen $examen_modif */
+            $json_array['examens_modifies'][] = $examen_modif->toArray();
+        }
+        $examens_nouveaux = $examen_model->getNewExamens($date);
+        foreach ($examens_nouveaux as $examen_nouveau) {
+            /** @var \APP\Entity\Examen $examen_nouveau */
+            $json_array['examens_nouveaux'][] = $examen_nouveau->toArray();
+        }
+        $examens_supprimes = $examen_model->getDeletedExamens($date);
+        foreach ($examens_supprimes as $examen_suppr) {
+            /** @var \APP\Entity\Examen $examen_suppr */
+            $json_array['examens_supprimes'][] = $examen_suppr->getIdExamen();
+        }
+
+        // Questions
+        $question_model = new QuestionModel();
+        $questions_modifies = $question_model->getUpdatedQuestions($date);
+        foreach ($questions_modifies as $question_modif) {
+            /** @var \APP\Entity\Question $question_modif */
+            $json_array['questions_modifiees'][] = $question_modif->toArray();
+        }
+        $questions_nouveaux = $question_model->getNewQuestions($date);
+        foreach ($questions_nouveaux as $question_nouveau) {
+            /** @var \APP\Entity\Question $question_nouveau */
+            $json_array['questions_nouvelles'][] = $question_nouveau->toArray();
+        }
+        $questions_supprimes = $question_model->getDeletedQuestions($date);
+        foreach ($questions_supprimes as $question_suppr) {
+            /** @var \APP\Entity\Question $question_suppr */
+            $json_array['questions_supprimees'][] = $question_suppr->getIdQuestion();
+        }
+
+        // Cours
+
+
+        return json_encode($json_array, JSON_NUMERIC_CHECK);
     }
 }
