00cd63ab037ae1114d7a54d542dd619e6b0efc51
samuelfr <samuel.frigout@gmail.com>
2015-06-12 17:18:39 +0200

    WebService - getUpdate : new fields date_update & is_empty

--- BackOffice_PHP/application/Module/WebService/Controller.php
+++ BackOffice_PHP/application/Module/WebService/Controller.php
@@ -16,6 +16,8 @@ class Controller extends \FSF\Controller
         $date = Date::blockToFr($date);
 
         $json_array = array(
+            "date_update"          => date('dmYHis'),
+            "is_empty"             => true,
             "themes_nouveaux"      => array(),
             "themes_modifies"      => array(),
             "examens_nouveaux"     => array(),
@@ -29,73 +31,94 @@ class Controller extends \FSF\Controller
             "cours_supprimes"      => array(),
         );
 
-        // Themes
-        $theme_model = new ThemeModel();
-        $themes_modifies = $theme_model->getUpdatedThemes($date);
-        foreach ($themes_modifies as $theme_modif) {
-            /** @var \APP\Entity\Theme $theme_modif */
-            $json_array['themes_modifies'][] = $theme_modif->toArray();
-        }
-        $themes_nouveaux = $theme_model->getNewThemes($date);
-        foreach ($themes_nouveaux as $theme_nouveau) {
-            /** @var \APP\Entity\Theme $theme_nouveau */
-            $json_array['themes_nouveaux'][] = $theme_nouveau->toArray();
-        }
-        $themes_supprimes = $theme_model->getDeletedThemes($date);
-        foreach ($themes_supprimes as $theme_suppr) {
-            /** @var \APP\Entity\Theme $theme_suppr */
-            $json_array['themes_supprimes'][] = $theme_suppr->getIdTheme();
+        if ($date != "") {
+            // Themes
+            $theme_model = new ThemeModel();
+            $themes_modifies = $theme_model->getUpdatedThemes($date);
+            foreach ($themes_modifies as $theme_modif) {
+                /** @var \APP\Entity\Theme $theme_modif */
+                $json_array['themes_modifies'][] = $theme_modif->toArray();
+            }
+            $themes_nouveaux = $theme_model->getNewThemes($date);
+            foreach ($themes_nouveaux as $theme_nouveau) {
+                /** @var \APP\Entity\Theme $theme_nouveau */
+                $json_array['themes_nouveaux'][] = $theme_nouveau->toArray();
+            }
+            $themes_supprimes = $theme_model->getDeletedThemes($date);
+            foreach ($themes_supprimes as $theme_suppr) {
+                /** @var \APP\Entity\Theme $theme_suppr */
+                $json_array['themes_supprimes'][] = $theme_suppr->getIdTheme();
+            }
+
+            // Examens
+            $examen_model = new ExamenModel();
+            $examens_modifies = $examen_model->getUpdatedExamens($date);
+            foreach ($examens_modifies as $examen_modif) {
+                /** @var \APP\Entity\Examen $examen_modif */
+                $json_array['examens_modifies'][] = $examen_modif->toArray();
+            }
+            $examens_nouveaux = $examen_model->getNewExamens($date);
+            foreach ($examens_nouveaux as $examen_nouveau) {
+                /** @var \APP\Entity\Examen $examen_nouveau */
+                $json_array['examens_nouveaux'][] = $examen_nouveau->toArray();
+            }
+            $examens_supprimes = $examen_model->getDeletedExamens($date);
+            foreach ($examens_supprimes as $examen_suppr) {
+                /** @var \APP\Entity\Examen $examen_suppr */
+                $json_array['examens_supprimes'][] = $examen_suppr->getIdExamen();
+            }
+
+            // Questions
+            $question_model = new QuestionModel();
+            $questions_modifies = $question_model->getUpdatedQuestions($date);
+            foreach ($questions_modifies as $question_modif) {
+                /** @var \APP\Entity\Question $question_modif */
+                $json_array['questions_modifiees'][] = $question_modif->toArray();
+            }
+            $questions_nouveaux = $question_model->getNewQuestions($date);
+            foreach ($questions_nouveaux as $question_nouveau) {
+                /** @var \APP\Entity\Question $question_nouveau */
+                $json_array['questions_nouvelles'][] = $question_nouveau->toArray();
+            }
+            $questions_supprimes = $question_model->getDeletedQuestions($date);
+            foreach ($questions_supprimes as $question_suppr) {
+                /** @var \APP\Entity\Question $question_suppr */
+                $json_array['questions_supprimees'][] = $question_suppr->getIdQuestion();
+            }
+
+            // Cours
+            $cours_model = new \APP\Model\Cours();
+            $cours_nouveaux = $cours_model->getNewCours($date);
+            foreach ($cours_nouveaux as $cours_nouveau) {
+                /** @var \APP\Entity\Cours $cours_nouveau */
+                $json_array['cours_nouveaux'][] = $cours_nouveau->toArray();
+            }
+            $cours_supprimes = $cours_model->getDeletedCours();
+            foreach ($cours_supprimes as $cours_suppr) {
+                /** @var \APP\Entity\Cours $cours_suppr */
+                $json_array['cours_supprimes'][] = $cours_suppr->getIdCours();
+            }
         }
 
-        // Examens
-        $examen_model = new ExamenModel();
-        $examens_modifies = $examen_model->getUpdatedExamens($date);
-        foreach ($examens_modifies as $examen_modif) {
-            /** @var \APP\Entity\Examen $examen_modif */
-            $json_array['examens_modifies'][] = $examen_modif->toArray();
+        $fields = array(
+            "themes_nouveaux",
+            "themes_modifies",
+            "examens_nouveaux",
+            "examens_modifies",
+            "questions_nouvelles",
+            "questions_modifiees",
+            "themes_supprimes",
+            "examens_supprimes",
+            "questions_supprimees",
+            "cours_nouveaux",
+            "cours_supprimes",
+        );
+        foreach ($fields as $field) {
+            if (!empty($json_array[$field])) {
+                $json_array["is_empty"] = false;
+                break;
+            }
         }
-        $examens_nouveaux = $examen_model->getNewExamens($date);
-        foreach ($examens_nouveaux as $examen_nouveau) {
-            /** @var \APP\Entity\Examen $examen_nouveau */
-            $json_array['examens_nouveaux'][] = $examen_nouveau->toArray();
-        }
-        $examens_supprimes = $examen_model->getDeletedExamens($date);
-        foreach ($examens_supprimes as $examen_suppr) {
-            /** @var \APP\Entity\Examen $examen_suppr */
-            $json_array['examens_supprimes'][] = $examen_suppr->getIdExamen();
-        }
-
-        // Questions
-        $question_model = new QuestionModel();
-        $questions_modifies = $question_model->getUpdatedQuestions($date);
-        foreach ($questions_modifies as $question_modif) {
-            /** @var \APP\Entity\Question $question_modif */
-            $json_array['questions_modifiees'][] = $question_modif->toArray();
-        }
-        $questions_nouveaux = $question_model->getNewQuestions($date);
-        foreach ($questions_nouveaux as $question_nouveau) {
-            /** @var \APP\Entity\Question $question_nouveau */
-            $json_array['questions_nouvelles'][] = $question_nouveau->toArray();
-        }
-        $questions_supprimes = $question_model->getDeletedQuestions($date);
-        foreach ($questions_supprimes as $question_suppr) {
-            /** @var \APP\Entity\Question $question_suppr */
-            $json_array['questions_supprimees'][] = $question_suppr->getIdQuestion();
-        }
-
-        // Cours
-        $cours_model = new \APP\Model\Cours();
-        $cours_nouveaux = $cours_model->getNewCours($date);
-        foreach ($cours_nouveaux as $cours_nouveau) {
-            /** @var \APP\Entity\Cours $cours_nouveau */
-            $json_array['cours_nouveaux'][] = $cours_nouveau->toArray();
-        }
-        $cours_supprimes = $cours_model->getDeletedCours();
-        foreach ($cours_supprimes as $cours_suppr) {
-            /** @var \APP\Entity\Cours $cours_suppr */
-            $json_array['cours_supprimes'][] = $cours_suppr->getIdCours();
-        }
-
 
         return json_encode($json_array, JSON_NUMERIC_CHECK);
     }
@@ -108,13 +131,13 @@ class Controller extends \FSF\Controller
         $image = $modelImage->get($id_image);
 
         if (!is_null($image)) {
-            $image_path = ROOT."/public/datas/images/" . $image->getNomImage();
+            $image_path = ROOT . "/public/datas/images/" . $image->getNomImage();
         }
 
         if (file_exists($image_path)) {
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename='.basename($image_path));
+            header('Content-Disposition: attachment; filename=' . basename($image_path));
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
             header('Pragma: public');
@@ -123,6 +146,7 @@ class Controller extends \FSF\Controller
             exit;
         }
     }
+
     function getCours()
     {
         $id_cours = $this->getRequest()->get("id");
@@ -132,13 +156,13 @@ class Controller extends \FSF\Controller
         $cours = $modelCours->get($id_cours);
 
         if (!is_null($cours)) {
-            $cours_path = ROOT."/public/datas/cours/" . $cours->getNomPdf();
+            $cours_path = ROOT . "/public/datas/cours/" . $cours->getNomPdf();
         }
 
         if (file_exists($cours_path)) {
             header('Content-Description: File Transfer');
             header('Content-Type: application/octet-stream');
-            header('Content-Disposition: attachment; filename='.basename($cours_path));
+            header('Content-Disposition: attachment; filename=' . basename($cours_path));
             header('Expires: 0');
             header('Cache-Control: must-revalidate');
             header('Pragma: public');
